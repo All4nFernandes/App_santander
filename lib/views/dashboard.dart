915af61f5d8ae7932339e8657eb0630075890f9c
Senac Hub - Sala 203 <hubsala203@aluno.ms.senac.br>
2025-07-07 21:32:11 -0400

    Conectando com a API

--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -9,117 +9,131 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   bool cardAberto = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height:
-                cardAberto
-                    ? MediaQuery.of(context).size.height * 0.50
-                    : MediaQuery.of(context).size.height * 0.36,
-            child: Stack(
-              children: [
-                Image.asset("fundo_dashboard.png"),
-                Padding(
-                  padding: const EdgeInsets.only(right: 12),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Image.asset("teste3.png", width: 150),
-                      Row(
-                        children: [
-                          Icon(Icons.search_outlined, color: Colors.white),
-                          Icon(Icons.message_outlined, color: Colors.white),
-                          Icon(
-                            Icons.notifications_outlined,
-                            color: Colors.white,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  top: 150,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 12),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height:
+                  cardAberto
+                      ? MediaQuery.of(context).size.height * 0.50
+                      : MediaQuery.of(context).size.height * 0.36,
+              child: Stack(
+                children: [
+                  Image.asset("fundo_dashboard.png", fit: BoxFit.cover),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 8,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(
-                          "Olá Allan Fernandes",
-                          style: TextStyle(color: Colors.white, fontSize: 17),
-                        ),
-                        Text(
-                          "Ag 4333 Cc 014577-8",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
+                        Image.asset("teste3.png", width: 150),
+                        Row(
+                          children: [
+                            Icon(Icons.search_outlined, color: Colors.white),
+                            SizedBox(width: 8),
+                            Icon(Icons.message_outlined, color: Colors.white),
+                            SizedBox(width: 8),
+                            Icon(
+                              Icons.notifications_outlined,
+                              color: Colors.white,
+                            ),
+                          ],
                         ),
                       ],
                     ),
                   ),
-                ),
-                Positioned(
-                  top: MediaQuery.of(context).size.height * 0.26,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 12, right: 12),
-                    child: Card(
-                      elevation: 10,
-                      color: Colors.white,
-                      child: Container(
-                        padding: EdgeInsets.all(12),
-                        width: MediaQuery.of(context).size.width * 0.93,
-                        height: cardAberto ? null : null,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    Icon(
-                                      Icons.monetization_on,
-                                      color: Colors.red,
+                  Positioned(
+                    top: 145,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Olá, Allan Fernandes",
+                            style: TextStyle(color: Colors.white, fontSize: 17),
+                          ),
+                          Text(
+                            "Ag 4333 Cc 014577-8",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: MediaQuery.of(context).size.height * 0.26,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      child: Card(
+                        elevation: 10,
+                        color: Colors.white,
+                        child: Container(
+                          padding: EdgeInsets.all(12),
+                          width: MediaQuery.of(context).size.width * 0.93,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        Icons.monetization_on_outlined,
+                                        color: Colors.red,
+                                        size: 30,
+                                      ),
+                                      SizedBox(width: 10),
+                                      Text(
+                                        "Saldo disponível",
+                                        style: TextStyle(fontSize: 16),
+                                      ),
+                                    ],
+                                  ),
+                                  IconButton(
+                                    icon: Icon(
+                                      cardAberto
+                                          ? Icons.keyboard_arrow_up
+                                          : Icons.keyboard_arrow_down,
                                       size: 30,
                                     ),
-                                    SizedBox(width: 10),
-                                    Text(
-                                      "Saldo disponivel",
-                                      style: TextStyle(fontSize: 16),
-                                    ),
-                                  ],
-                                ),
-                                IconButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      cardAberto = !cardAberto;
-                                    });
-                                  },
-                                  icon:
-                                      cardAberto
-                                          ? Icon(Icons.keyboard_arrow_up)
-                                          : Icon(Icons.keyboard_arrow_down),
-                                ),
-                              ],
-                            ),
-                            cardAberto
-                                ? Column(
+                                    onPressed: () {
+                                      setState(() {
+                                        cardAberto = !cardAberto;
+                                      });
+                                    },
+                                  ),
+                                ],
+                              ),
+                              if (cardAberto)
+                                Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     Text(
                                       "R\$ 1.000.000,00",
                                       style: TextStyle(
                                         fontWeight: FontWeight.bold,
-                                        fontSize: 22,
+                                        fontSize: 20,
                                       ),
                                     ),
-                                    Text("Saldo + Limite: 143,00"),
+                                    SizedBox(height: 4),
+                                    Text(
+                                      "Saldo + Limite: 143,00",
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                    SizedBox(height: 4),
                                     Text(
                                       "Entenda seu limite",
                                       style: TextStyle(
@@ -143,18 +157,18 @@ class _DashboardState extends State<Dashboard> {
                                       ],
                                     ),
                                   ],
-                                )
-                                : Container(),
-                          ],
+                                ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
