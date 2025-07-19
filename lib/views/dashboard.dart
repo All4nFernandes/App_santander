f2dd01fb6243f493baa7d53078c178202b787ce8
All4nfernandes <allandossantosfernandes@gmail.com>
2025-07-18 20:16:13 -0400

    Fazendo a tela definir transferencia

--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -1,4 +1,4 @@
-import 'package:app_santander/views/pix1.dart';
+import 'package:app_santander/views/pix_1.dart';
 import 'package:carousel_slider/carousel_controller.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
@@ -22,9 +22,9 @@ class _DashboardState extends State<Dashboard> {
   ];
 
   final List<dynamic> cardsTransf = [
-    {"titulo": "Pix", "Icon": Icons.pix, "route": Pix1()},
-    {"titulo": "Pix", "Icon": Icons.lock_open, "route": Pix1()},
-    {"titulo": "Pix", "Icon": Icons.pix, "route": Pix1()}
+    {"titulo": "Pix", "icon": Icons.pix, "route": Pix1()},
+    {"titulo": "ID Santander", "icon": Icons.lock_open, "route": Pix1()},
+    {"titulo": "Pix", "icon": Icons.pix, "route": Pix1()}
   ];
 
   @override
@@ -32,6 +32,7 @@ class _DashboardState extends State<Dashboard> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               height: cardAberto
@@ -202,14 +203,17 @@ class _DashboardState extends State<Dashboard> {
               height: 130,
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
-                itemCount: 10,
+                itemCount: cardsTransf.length,
                 itemBuilder: (context, index) {
                   return Padding(
                     padding: const EdgeInsets.all(4.0),
                     child: GestureDetector(
                       onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => cardsTransf[index]['route']));
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => cardsTransf[index]['route'],
+                          ),
+                        );
                       },
                       child: Card(
                         child: Container(
@@ -217,11 +221,11 @@ class _DashboardState extends State<Dashboard> {
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              Icon(cardsTransf[index]["icon"]),
+                              Icon(cardsTransf[index]['icon']),
                               SizedBox(
                                 height: 10,
                               ),
-                              Text(cardsTransf[index]['Pix']),
+                              Text(cardsTransf[index]['titulo']),
                             ],
                           ),
                         ),
@@ -232,70 +236,77 @@ class _DashboardState extends State<Dashboard> {
               ),
             ),
             // column dos cards vermelhos
-            Column(children: [
-              CarouselSlider(
-                items: [
-                  Container(
-                    height: 150,
-                    width: double.maxFinite,
-                    color: Colors.red,
-                    child: Column(
-                      children: [
-                        Text('Conta PJ + fácil'),
-                        Text(
-                            'Conta MEI c/ 1 ano grátis em poucos\ncliques. Abra já'),
-                      ],
+            Column(
+              children: [
+                CarouselSlider(
+                  items: [
+                    Container(
+                      height: 150,
+                      width: double.maxFinite,
+                      color: Colors.red,
+                      child: Column(
+                        children: [
+                          Text('Conta PJ + fácil'),
+                          Text(
+                              'Conta MEI c/ 1 ano grátis em poucos\ncliques. Abra já'),
+                        ],
+                      ),
                     ),
-                  ),
-                  Text('2'),
-                  Text('3'),
-                ],
-                carouselController: _controller,
-                options: CarouselOptions(
-                    autoPlay: true,
-                    enlargeCenterPage: true,
-                    height: 80,
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        _current = index;
-                      });
-                    }),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: stringList.asMap().entries.map((entry) {
-                  return GestureDetector(
-                    onTap: () => _controller.animateToPage(entry.key),
-                    child: Container(
-                      width: 12.0,
-                      height: 12.0,
-                      margin:
-                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: (Theme.of(context).brightness ==
-                                      Brightness.dark
-                                  ? Colors.white
-                                  : Colors.black)
-                              .withOpacity(_current == entry.key ? 0.9 : 0.4)),
-                    ),
-                  );
-                }).toList(),
-              ),
-            ]),
+                    Text('2'),
+                    Text('3'),
+                  ],
+                  carouselController: _controller,
+                  options: CarouselOptions(
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      height: 80,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          _current = index;
+                        });
+                      }),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: stringList.asMap().entries.map((entry) {
+                    return GestureDetector(
+                      onTap: () => _controller.animateToPage(entry.key),
+                      child: Container(
+                        width: 12.0,
+                        height: 12.0,
+                        margin: EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 4.0),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color:
+                                (Theme.of(context).brightness == Brightness.dark
+                                        ? Colors.white
+                                        : Colors.black)
+                                    .withOpacity(
+                                        _current == entry.key ? 0.9 : 0.4)),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
             Padding(
-              padding: const EdgeInsets.only(left: 12.0, top: 12.0, bottom: 12),
+              padding:
+                  const EdgeInsets.only(left: 12.0, top: 12.0, bottom: 12.0),
               child: Text(
                 "Cartões",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            Card(
-              color: Color.fromARGB(255, 147, 145, 146),
-              child: Container(
-                height: 250,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Card(
+                color: Color.fromARGB(255, 147, 145, 146),
+                child: Container(
+                  height: 180,
+                  padding: const EdgeInsets.all(12.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
@@ -303,56 +314,65 @@ class _DashboardState extends State<Dashboard> {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.credit_card,
-                                    color: Colors.white,
-                                  ),
-                                  SizedBox(
-                                    width: 12,
-                                  ),
-                                  Text(
-                                    "Cartão Final",
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Text(
-                                    "2244",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white),
-                                  ),
-                                ],
+                              Icon(
+                                Icons.credit_card,
+                                color: Colors.white,
+                              ),
+                              SizedBox(width: 12),
+                              Text(
+                                "Cartão final",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                "2244",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
                               ),
                             ],
-                            Icon(
-                              Icons.keyboard_arrow_down,
+                          ),
+                          Icon(
+                            Icons.keyboard_arrow_down,
+                            color: Colors.white,
+                          )
+                        ],
+                      ),
+                      Text(
+                        "Santander Elite Master",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Limite disponível",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            "R\$ 5.000,00",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
                               color: Colors.white,
                             ),
                           ),
                         ],
                       ),
-                      Text(
-                        "Satander Elite Master",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Limite disponível"),
-                          Text(
-                            "R\$5,000",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      ),
                       Divider(),
-                      Text("Ver Fatura"),
+                      Text(
+                        "Ver fatura",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -364,12 +384,15 @@ class _DashboardState extends State<Dashboard> {
               alignment: Alignment.center,
               width: double.maxFinite,
               decoration: BoxDecoration(
-                  border: Border.all(color: Colors.red),
-                  borderRadius: BorderRadius.all(Radius.circular(5))),
-              padding: EdgeInsets.all(8.0),
+                  border: Border.all(
+                    color: Colors.red,
+                  ),
+                  borderRadius: BorderRadius.circular(5)),
               child: Text(
                 "Cartão Online",
-                style: TextStyle(color: Colors.red),
+                style: TextStyle(
+                  color: Colors.red,
+                ),
               ),
             )
           ],
